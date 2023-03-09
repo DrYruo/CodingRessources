3ae15227823c39e258d95ace0f311ba29468615c
DrYruo <oury.jordan@hotmail.com>
2023-03-09 02:20:05 +0100

    fin du tuto

--- React/reactWorld/src/components/Logo.js
+++ React/reactWorld/src/components/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Logo = () => {
-    return (
-        <div className="logo">
-            {/*Les balises IMG en react prennent leur source "./" depuis le dossier "public"*/}
-            <img src="./logo192.png" alt="logo react"/>
-            <h3>React World</h3>
-        </div>
-    );
+  return (
+    <div className="logo">
+      {/*Les balises IMG en react prennent leur source "./" depuis le dossier "public"*/ }
+      <img src="./logo192.png" alt="logo react"/>
+      <h3>React World</h3>
+    </div>
+  );
 };
 
 export default Logo;
